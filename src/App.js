d48dd520f6a71d2973ef8e74312cc4b5fdee8747
JohnMnyika <jmnyika514@gmail.com>
2023-05-23 02:36:25 +0300

    created create and read using axios on react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function UserList() {
+    const [users, setUsers] = useState([]);
+    const [newUserName, setNewUserName] = useState('');
+    const [newUserEmail, setNewUserEmail] = useState('');
+
+    // Fetch the list of users from the API
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching users:', error);
+            });
+    }, []);
+
+    // Create a new user
+    const createUser = () => {
+        const newUser = {
+            name: newUserName,
+            email: newUserEmail
+        };
+
+        axios.post('https://jsonplaceholder.typicode.com/users', newUser)
+            .then(response => {
+                setUsers([...users, response.data]);
+                setNewUserName('');
+                setNewUserEmail('');
+            })
+            .catch(error => {
+                console.error('Error creating user:', error);
+            });
+    };
+
+    return (
+        <div className="crud">
+            <h1 className="h1">User List</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name} - {user.email}</li>
+                ))}
+            </ul>
+
+            <h2>Add New User</h2>
+            <input
+                type="text"
+                placeholder="Name"
+                value={newUserName}
+                onChange={e => setNewUserName(e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Email"
+                value={newUserEmail}
+                onChange={e => setNewUserEmail(e.target.value)}
+            />
+            <button onClick={createUser}>Add User</button>
+        </div>
+    );
 }
 
-export default App;
+export default UserList;
