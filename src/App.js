acd658767e95e508e300d219b903f3a6cb60a6b6
JohnMnyika <jmnyika514@gmail.com>
2023-05-23 18:43:49 +0300

    Added Tailwind CSS and redesigned the page with a table

--- src/App.js
+++ src/App.js
@@ -36,39 +36,54 @@ function UserList() {
     };
 
     return (
-        <div>
-            <h1>User List</h1>
-            <table>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                </tr>
-                </thead>
-                <tbody>
-                {users.map(user => (
-                    <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+        <div className="flex justify-center items-center h-screen">
+            <div className="container mx-auto p-4">
+                <div className="flex justify-center">
+                    <h2 className="text-2xl font-bold">CRUD with Axios and React</h2>
+                </div>
+                <div className="flex justify-center mt-4">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={newUserName}
+                        onChange={e => setNewUserName(e.target.value)}
+                        className="border rounded-l px-4 py-2 outline-none"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        value={newUserEmail}
+                        onChange={e => setNewUserEmail(e.target.value)}
+                        className="border rounded-r px-4 py-2 outline-none"
+                    />
+                    <button
+                        onClick={createUser}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 rounded"
+                    >
+                        Create
+                    </button>
+                </div>
 
-            <h2>Add New User</h2>
-            <input
-                type="text"
-                placeholder="Name"
-                value={newUserName}
-                onChange={e => setNewUserName(e.target.value)}
-            />
-            <input
-                type="text"
-                placeholder="Email"
-                value={newUserEmail}
-                onChange={e => setNewUserEmail(e.target.value)}
-            />
-            <button onClick={createUser}>Add User</button>
+                <h1 className="text-3xl font-bold mb-4 text-center">User List</h1>
+                <div className="flex flex-col items-center">
+                    <table className="table-auto">
+                        <thead>
+                        <tr>
+                            <th className="px-4 py-2">Name</th>
+                            <th className="px-4 py-2">Email</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {users.map(user => (
+                            <tr key={user.id}>
+                                <td className="border px-4 py-2">{user.name}</td>
+                                <td className="border px-4 py-2">{user.email}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
